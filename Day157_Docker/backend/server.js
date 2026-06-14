8ab375056e767110e8354774624668a8f073170e
sagarsinha668 <144223600+sagarsinha668@users.noreply.github.com>
2026-06-14 13:47:42 +0530

    as

--- Day157_Docker/backend/server.js
+++ Day157_Docker/backend/server.js
@@ -1,9 +1,13 @@
 // docker common commands lines.
 // docker build -t docker-test .
 // docker run -p 3000:3000 docker-test
-import express from "express"
+// docker ps 
+// docker stop <container_id> to stop the container
+// docker rm <container_id> to remove the container
+// docker compose up -d to start the container in detached mode
 
-const app = express()
+import express from "express"
+import app from "./src/app.js"
 
 app.get("/", (req, res) => {
     res.send("Docker Testing")
